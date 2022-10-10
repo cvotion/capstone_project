023bdc7cf1e67a141e1adb1dbcfbe5b98ba12b71
Christian Votion <christianvotion@gmail.com>
2022-10-10 15:59:20 -0500

    created routes

--- client/src/index.js
+++ client/src/index.js
@@ -15,9 +15,20 @@ import Favorites from './components/Favorites'
 import Login from './components/Login'
 import Register from './components/Register'
 
+let store = createStore(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <Router>
+        <Switch>
+          <Route path="/" element={<App/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
+          <Route path="/favorites" element={<Favorites/>} />
+        </Switch>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
