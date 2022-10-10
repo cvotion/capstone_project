36e59f10e7949f53616388872e126d012b994c60
Christian Votion <christianvotion@gmail.com>
2022-10-10 16:17:30 -0500

    added store to provider

--- client/src/index.js
+++ client/src/index.js
@@ -20,7 +20,7 @@ let store = createStore(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DE
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <Router>
         <Switch>
           <Route path="/" element={<App/>} />
