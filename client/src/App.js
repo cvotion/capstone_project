4e810cca7a2027c9315de291efb1971b7f5a0372
Thomas Scheltema <scheltemat@yahoo.com>
2022-10-11 09:34:06 -0500

    map on screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,43 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import 'mapbox-gl/dist/mapbox-gl.css'
 import mapboxgl from 'mapbox-gl'
 import {keys} from './secret.js'
 
 const App = () => {
 
-  mapboxgl.accessToken = `${keys.mapboxToken}`;
-  const map = new mapboxgl.Map({
-      container: 'map', // container ID
-      style: 'mapbox://styles/mapbox/streets-v11', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9 // starting zoom
-  });
+  useEffect(() => {
 
-  map.on('load', () => {
-    map.addLayer({
-        id: 'terrain-data',
-        type: 'line',
-        source: {
-            type: 'vector',
-            url: 'mapbox://mapbox.mapbox-terrain-v2'
-        },
-        'source-layer': 'contour'
+    mapboxgl.accessToken = `${keys.mapboxToken}`;
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [-95.37, 29.76], // starting position [lng, lat]
+        zoom: 9 // starting zoom
     });
-  });
+
+    // map.on('load', () => {
+    //   map.addLayer({
+    //       id: 'terrain-data',
+    //       type: 'line',
+    //       source: {
+    //           type: 'vector',
+    //           url: 'mapbox://mapbox.mapbox-terrain-v2'
+    //       },
+    //       'source-layer': 'contour'
+    //   });
+    // });
+
+    // return () =>{
+    //   map.remove()
+    // }
+
+  }, [])
+  
 
   return (
     <>
 
-      <div id='map'></div>  
+      <div id='map' style={{width: "700px", height: "400px"}}></div>  
     
     </>
   )
