45720229d2179bdf539bd65055e9fed82cc105fc
Nauman Khan <thetopofsoup@gmail.com>
2022-10-13 14:52:07 -0400

    loginFix

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -37,7 +37,7 @@ const Login = () => {
       setDisplayLoginError("Invalid password")
     }
     dispatch(signIn({email, password}, ()=>{
-      navigate('/favorites')
+      navigate('/')
       setDisplayLoginError("")
     }))
 
