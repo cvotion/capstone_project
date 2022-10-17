12f21b82b305a581a1094a62b831826032ac40a4
Thomas Scheltema <scheltemat@yahoo.com>
2022-10-17 10:39:53 -0500

    home page style

--- client/src/App.js
+++ client/src/App.js
@@ -232,10 +232,13 @@ const App = () => {
   
   return (
     <>
+      <div className='map-container'>
+        <div id='map' style={{width: "100%", height: "100%"}}></div> 
+      </div>
 
-      <div id='map' style={{width: "700px", height: "400px"}}></div> 
-
-      <div id="instructions"></div>
+      <div className='instructions-container'>
+        <div id="instructions"></div>
+      </div>
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
